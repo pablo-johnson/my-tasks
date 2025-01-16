0963a434d1847b720924913700819a3aff1c6a9a
Pablo Johnson <pablo.88j@gmail.com>
2025-01-16 19:35:08 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/johnson/my_tasks/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.johnson.my_tasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main_prod.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:my_tasks/firebase_options_qa.dart';
+import 'package:my_tasks/main.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,  // Point to the Prod Firebase config
+  );
+  runApp(MyApp());
+}
